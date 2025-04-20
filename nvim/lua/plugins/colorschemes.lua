40f351919a8319a4556dac455bfa13ef59244882
a-pillow <kaluhblee@gmail.com>
2025-04-20 00:38:03 -0500

    update

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -13,5 +13,21 @@ return {
       colorscheme = "everforest",
       background = "dark",
     },
+    {
+      "cdmill/neomodern.nvim",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        require("neomodern").setup({
+          theme = "roseprime",
+        })
+      end,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "default",
+    },
   },
 }
